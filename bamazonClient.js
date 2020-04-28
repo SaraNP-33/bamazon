9ebd9118507781af8f7503d68e691b70acf123fe
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-04-28 13:42:14 -0700

    exit function working, raw json object being displayed

--- bamazonClient.js
+++ bamazonClient.js
@@ -1,6 +1,9 @@
+//connect dependicies in package JSON to the file via require
 var mysql= require("mysql");
 
 var inquirer= require("inquirer");
+
+// establish mysql connection to bamazon database
 
 var connection= mysql.createConnection({
     host:"localhost",
@@ -13,5 +16,52 @@ var connection= mysql.createConnection({
 connection.connect(function(err){
     if(err) throw err;
     console.log("Connected as id", connection.threadId);
-    connection.end();
+   options();
 });
+
+// function that brings up the options menu
+
+function options(){
+    inquirer
+    .prompt({
+        type:"list",
+        name:"option",
+        message:"What would you like to do?",
+        choices:["View Products", "Buy a Product", "Exit"]
+    })
+    .then(function(answer){
+
+   
+    switch(answer.option){
+        case "View Products": 
+        displayTable();
+        break;
+        case "Buy a Product": 
+        buyProd();
+        break;
+        case "Exit":
+            exit();
+        break;
+    }
+})
+ 
+};
+
+function displayTable(){
+    
+connection.query("SELECT * FROM products", function(err,res){
+    if(err) throw(err)
+   console.log(res);
+    
+    options();
+})
+}
+
+
+
+
+//function that ends the connection
+function exit(){
+    connection.end();
+    console.log("Thank you for your visit!")
+}
