2ca73e53aedd5c74194e32a94ac8f7487dabdaef
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2020-04-29 10:36:27 -0700

    all functions working

--- bamazonClient.js
+++ bamazonClient.js
@@ -2,7 +2,9 @@
 var mysql= require("mysql");
 
 var inquirer= require("inquirer");
+
 var Table = require("cli-table3");
+var colors =require("colors");
 
 // establish mysql connection to bamazon database
 
@@ -52,34 +54,79 @@ function options(){
 function displayTable(){
     //CLI-table3 language to create a custom table
     var table = new Table({
+        head:[colors.green("ID"), colors.brightBlue("Product"),colors.magenta("Price"), colors.cyan("Department"),colors.yellow("Stock Quantity")],
         chars: { 'top': '═' , 'top-mid': '╤' , 'top-left': '╔' , 'top-right': '╗'
                , 'bottom': '═' , 'bottom-mid': '╧' , 'bottom-left': '╚' , 'bottom-right': '╝'
                , 'left': '║' , 'left-mid': '╟' , 'mid': '─' , 'mid-mid': '┼'
                , 'right': '║' , 'right-mid': '╢' , 'middle': '│' }
       });
     // ask SQL to send the info from the table    
-connection.query("SELECT * FROM products", function(err,res){
+ connection.query("SELECT * FROM bamazon_db.products", function(err,res){
     if(err) throw(err)
+    // console.log(res);
     //create a loop to eiterate through the result from the query
    for(var i=0; i<res.length;i++){
-      //assign a variable to hold the result from the query 
+//       //assign a variable to hold the result from the query 
     var items= res[i];
     //transform the object sent from mySQL table into an array
-       items=Object.values(items)
+     items=Object.values(items)
     //turn that array into a table that can be displayed in the terminal
     table.push(items);
-       console.log(table.toString());
-   }
+    
+  }
+  console.log(table.toString());
     //give the user the options to do othr things. 
     options();
 })
 }
-
+function buyProd(){
+    inquirer
+    .prompt([{
+        type:"input",
+        name:"product",
+        message:"What's the ID of the product you want to purchase?"
+    },
+    {
+        type:"input",
+        name:"quantity",
+        message:"How many items would you like to purchase?"
+    },
+    {
+        type:"confirm",
+        name:"complete",
+        message:"Complete Purchase?",
+        default:true
+    }
+  
+])
+  .then(function(ans){
+    var prod=ans.product;
+    var qty=ans.quantity;
+    connection.query("SELECT * FROM products WHERE id= "+ prod, function(err,res){
+        if(err) throw(err)
+        //console.log(res);
+        if(qty<=res[0].stock_quantity){
+            var updateQuery= "UPDATE products SET stock_quantity= stock_quantity -" + qty + " WHERE id=" + prod
+            console.log(updateQuery)
+            connection.query(updateQuery, function(err,result){
+                if(err)throw(err)
+                console.log("Thank you for your purchase. Your total price is:", (res[0].price * qty).toFixed(2));  
+                exit();
+            })
+            
+        }else{
+            console.log("Sorry, insufficient quantity");
+            exit();
+        }
+        
+    })
+  })
+    }
 
 
 
 //function that ends the connection
 function exit(){
     connection.end();
-    console.log("Thank you for your visit!")
+    //console.log("Thank you for your visit!")
 }
